9bb4d129901b5203a8c55afc46b76f4aacd0a58b
Ketcia <leticiarafael98@gmail.com>
2022-05-03 08:37:15 -0300

    03/04

--- controller/Form.php
+++ controller/Form.php
@@ -2,30 +2,87 @@
 class Form
 {
   private $message = "";
+  private $error = "";
   public function __construct(){
     Transaction::open();
   }
   public function controller()
   {
     $form = new Template('view/form.html');
+    $form->set("id", "");
+    $form->set("marca", "");
+    $form->set("motor", "");
+    $form->set("modelo", "");
     $this->message = $form->saida();
   }
-  public function salvar(){
-    if(isset($_POST["marca"]) && isset($_POST["modelo"]) && isset($_POST["motor"])){
+  public function salvar()
+  {
+    if (isset($_POST["marca"]) && isset($_POST["modelo"]) && isset($_POST["motor"])) {
       try {
         $conexao = Transaction::get();
         $veiculo = new Crud("veiculo");
         $marca = $conexao->quote($_POST["marca"]);
         $modelo = $conexao->quote($_POST["modelo"]);
         $motor = $conexao->quote($_POST["motor"]);
-        $resultado = $veiculo->insert("marca, modelo, motor", "$marca, $modelo, $motor");
+        if (empty($_POST["id"])) {
+          $veiculo->insert(
+            "marca, modelo, motor",
+            "$marca, $modelo, $motor"
+          );
+        } else {
+          $id = $conexao->quote($_POST["id"]);
+          $veiculo->update(
+            "marca = $marca, modelo = $modelo, motor = $motor",
+            "id = $id"
+          );
+        }
+        $this->message = $veiculo->getMessage();
+        $this->error = $veiculo->getError();
       } catch (Exception $e) {
         echo $e->getMessage();
+        
       }
     }
   }
-  public function getMessage(){
+  public function editar()
+  {
+    if (isset($_GET["id"])) {
+      try {
+        $conexao = Transaction::get();
+        $id = $conexao->quote($_GET["id"]);
+        $veiculo = new Crud("veiculo");
+        $resultado = $veiculo->select("*", "id = $id");
+        if (!$veiculo->getError()) {
+          $form = new Template("view/form.html");
+          foreach ($resultado[0] as $cod => $valor) {
+            $form->set($cod, $valor);
+          }
+          $this->message = $form->saida();
+        } else {
+          $this->message = $veiculo->getMessage();
+          $this->error = true;
+        }
+      } catch (Exception $e) {
+        $this->message = $e->getMessage();
+        $this->error = true;
+      }
+    }
+  }
+  public function getMessage()
+  {
+    if (is_string($this->error)) {
       return $this->message;
+    } else {
+      $msg = new Template("view/msg.html");
+      if ($this->error) {
+        $msg->set("cor", "danger");
+      } else {
+        $msg->set("cor", "success");
+      }
+      $msg->set("msg", $this->message);
+      $msg->set("uri", "?class=Tabela");
+      return $msg->saida();
+    }
   }
   public function __destruct()
   {
--- controller/Tabela.php
+++ controller/Tabela.php
@@ -2,36 +2,67 @@
 class Tabela
 {
     private $message = "";
+    private $error = "";
 
   public function __construct(){
       Transaction::open();
   }
   public function controller()
   {
-    Transaction::get();
+    try {
+      Transaction::get();
     $veiculo = new Crud("veiculo");
     $resultado = $veiculo->select();
     $tabela = new Template("view/tabela.html");
     if(is_array($resultado)>0){
       $tabela->set("linha", $resultado);
       $this->message = $tabela->saida();
+    }else{
+      $this->message = $veiculo->getMessage();
+        $this->error = $veiculo->getError();
     }
-
+    } catch (\Throwable $th) {
+      $this->message = $e->getMessage();
+      $this->error = true;
+    }
+    
   }
 
   public function remover()
   {
-    try {
-      $conexao = Transaction::get();
-      $id = $conexao->quote($_GET["id"]);
-      $veiculo = new Crud("veiculo");
-      $resultado = $veiculo->delete("id = $id");
-    } catch (Exception $e) {
-      echo $e->getMessage();
+    if(isset($_GET["id"])){
+      try {
+        $conexao = Transaction::get();
+        $id = $conexao->quote($_GET["id"]);
+        $veiculo = new Crud("veiculo");
+        $resultado = $veiculo->delete("id = $id");
+        $this->message = $veiculo->getMessage();
+        $this->error = $veiculo->getError();
+      } catch (Exception $e) {
+        $this->message = $e->getMessage();
+        $this->error = true;
+      }
+    }else{
+      $this->message = "Faltando parâmetro!";
+      $this->error = true;
     }
+    
   }
-  public function getMessage(){
-    return $this->message;
+  public function getMessage()
+  {
+    if (is_string($this->error)) {
+      return $this->message;
+    } else {
+      $msg = new Template("view/msg.html");
+      if ($this->error) {
+        $msg->set("cor", "danger");
+      } else {
+        $msg->set("cor", "success");
+      }
+      $msg->set("msg", $this->message);
+      $msg->set("uri", "?class=Tabela");
+      return $msg->saida();
+    }
   }
   public function __destruct()
   {
